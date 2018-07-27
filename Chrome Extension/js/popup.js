e15bdaefd9abda174fdd3c7e6a2c1d19b25af6f9
Daniel Gonzalez <daniel.gonzalez@grainger.com>
2018-07-27 15:16:41 -0500

    clean popup to just alert URL

--- Chrome Extension/js/popup.js
+++ Chrome Extension/js/popup.js
@@ -1,18 +1,22 @@
 // Loads the Swarm API pointing to the official gateway
-const swarm = require("swarm-js").at("http://swarm-gateways.net");
+//const swarm = require("swarm-js").at("http://swarm-gateways.net");
 
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('triggerArchchain');
     checkPageButton.addEventListener('click', function() {
   
       chrome.tabs.getSelected(null, function(tab) {
-        console.log('Extension grabbed URL: ' + tab.url);
+        alert('URL: ' + tab.url);
 
         const file = tab.url;
+        /*
+        Swarm needs HALP!
+
         swarm.upload(file).then(hash => {
           console.log("Uploaded file. Address:", hash);
           alert('The Swarm Hash is:' + hash);
         })
+        */
       });
     }, false);
   }, false);
