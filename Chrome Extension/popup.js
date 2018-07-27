dce92dca71c6c89ef811ad56103cb1dc18d903aa
Daniel Gonzalez <daniel.gonzalez@grainger.com>
2018-07-27 13:42:06 -0500

    extension and website organization

---
+++ Chrome Extension/popup.js
@@ -0,0 +1,17 @@
+// Loads the Swarm API pointing to the official gateway
+const swarm = require("swarm-js").at("http://swarm-gateways.net");
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('triggerArchchain');
+    checkPageButton.addEventListener('click', function() {
+  
+      chrome.tabs.getSelected(null, function(tab) {
+        console.log('Extension grabbed URL: ' + tab.url);
+
+        const file = tab.url;
+        swarm.upload(file).then(hash => {
+          console.log("Uploaded file. Address:", hash);
+        })
+      });
+    }, false);
+  }, false);
--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('checkPage');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        alert(window.location.href);
-      });
-    }, false);
-  }, false);
